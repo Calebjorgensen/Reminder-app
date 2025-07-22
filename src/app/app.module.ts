bc6e21727f1938d3a89bd0b8b0c9a3dfa6165a5c
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-07-22 11:30:51 -0700

    first components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReminderListComponent } from './reminders/reminder-list/reminder-list.component';
+import { ReminderFormComponent } from './reminders/reminder-form/reminder-form.component';
+import { ReminderComponent } from './reminders/reminder/reminder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReminderListComponent,
+    ReminderFormComponent,
+    ReminderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/reminders/reminder-form/reminder-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reminder-form',
+  standalone: false,
+  templateUrl: './reminder-form.component.html',
+  styleUrl: './reminder-form.component.css'
+})
+export class ReminderFormComponent {
+
+}
